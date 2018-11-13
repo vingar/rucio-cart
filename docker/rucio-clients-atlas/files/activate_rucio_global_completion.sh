7760cd30f1b15e2147be81792c4a4c32128132c8
Vincent Garonne <vgaronne@gmail.com>
2018-11-13 11:57:12 +0100

    Add ARC clients; cleanups; Add template file and configuration generation

--- docker/rucio-clients-atlas/files/activate_rucio_global_completion.sh
+++
@@ -1,9 +0,0 @@
-# Authors:
-# - Vincent Garonne, <vgaronne@gmail.com>, 2018
-
-shopt -s checkwinsize
-
-# Script to enable shell completion on the rucio commands
-
-eval "$(register-python-argcomplete rucio)"
-eval "$(register-python-argcomplete rucio-admin)"
